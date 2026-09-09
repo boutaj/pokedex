d89885132321c592e9c80bae94c94edf6727f859
boutaj <boutajmouad@proton.me>
2026-09-09 03:33:47 +0200

    feat: build the bones of the game REPL

--- main.go
+++ main.go
@@ -1,9 +1,17 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
 func main() {
-	fmt.Println(cleanInput("Hello World!"))
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("Pokedex > ")
+		scanner.Scan()
+		text := cleanInput(scanner.Text())
+		fmt.Printf("Your command was: %s\n", text[0])
+	}
 }
